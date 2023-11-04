5a408e1200270a00871038f524fd350b8aa5b2f0
Abbas Momeni <a.momeni@zoot.com>
2023-11-04 15:09:54 +0330

    ref: interface

--- src/Contracts/SmsInterface.php
+++ src/Contracts/SmsInterface.php
@@ -2,11 +2,13 @@
 
 namespace Celysium\MessageBroker\Contracts;
 
+use Illuminate\Http\Client\Response;
+
 interface SmsInterface
 {
-    public function receiver(string $to);
+    public function to(string $number): void;
 
-    public function send(array $data): void;
+    public function send(array $data): Response;
 
-    public function otp(array $data): void;
+    public function otp(array $data): Response;
 }

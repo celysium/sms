f30c2c63bd4ffea8b972a7dc3f1acaa92aa5c9e8
Abbas Momeni <a.momeni@zoot.com>
2023-11-04 16:08:09 +0330

    feat: Payam.php

--- src/Drivers/Payam.php
+++ src/Drivers/Payam.php
@@ -3,22 +3,62 @@
 namespace Celysium\MessageBroker\Drivers;
 
 use Celysium\MessageBroker\Contracts\SmsInterface;
+use Illuminate\Http\Client\Response;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
+
 
 class Payam implements SmsInterface
 {
+    private string $to = '';
 
-    public function receiver(string $to)
+
+    public function to(string $number): void
     {
-        // TODO: Implement receiver() method.
+        $this->to = $number;
     }
 
-    public function send(array $data): void
+    public function send(array $data): Response
     {
-        // TODO: Implement send() method.
+        $data = [
+            'organization' => config('sms.payam.organization'),
+            'username' => config('sms.payam.username'),
+            'password' => config('sms.payam.password'),
+            'method' => 'send',
+            'messages' => [
+                [
+                    'sender' => config('sms.payam.sender'),
+                    'recipient' => $this->to,
+                    'body' => $data['message'],
+                    'customerId' => $this->to
+                ]
+            ]
+        ];
+        Log::debug('PAYAM_SMS_PREPARE_DATA: ', $data);
+        $response = Http::withHeaders(['Content-Type' => 'application/json'])->post(config('sms.payam.api'), $data);
+        Log::debug('PAYAM_SMS_RESPONSE: ', $response->json());
+        return $response;
     }
 
-    public function otp(array $data): void
+    public function otp(array $data): Response
     {
-        // TODO: Implement otp() method.
+        $data = [
+            'organization' => config('sms.payam.organization'),
+            'username' => config('sms.payam.username'),
+            'password' => config('sms.payam.password'),
+            'method' => 'send',
+            'messages' => [
+                [
+                    'sender' => config('sms.payam.sender'),
+                    'recipient' => $this->to,
+                    'body' => $data['message'],
+                    'customerId' => $this->to
+                ]
+            ]
+        ];
+        Log::debug('PAYAM_SMS_PREPARE_DATA: ', $data);
+        $response = Http::withHeaders(['Content-Type' => 'application/json'])->post(config('sms.payam.api'), $data);
+        Log::debug('PAYAM_SMS_RESPONSE: ', $response->json());
+        return $response;
     }
 }

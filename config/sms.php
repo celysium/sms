3f6a7ae1fb5ee5c1f2048f2ce61df5262632814f
nasser <hekmati.nasser@gmail.com>
2023-11-30 19:22:16 +0330

    fix: sms driver

--- config/sms.php
+++ config/sms.php
@@ -4,11 +4,10 @@ return [
     'default' => env('SMS_DEFAULT_DRIVER', 'kavenegar'),
     'kavenegar' => [
         'api_key' => env('KAVENEGAR_KEY'),
-        'token' => env('KAVENEGAR_TOKEN')
     ],
     'payam' => [
         'api' => env('PAYAM_SMS_API', 'https://new.payamsms.com/services/rest/index.php'),
-        'organization' => env('PAYAM_SMS_ORGANIZATION', 'ayria'),
+        'organization' => env('PAYAM_SMS_ORGANIZATION'),
         'username' => env('PAYAM_SMS_USERNAME'),
         'password' => env('PAYAM_SMS_PASSWORD'),
         'sender' => env('PAYAM_SMS_SENDER'),
--- src/Sms.php
+++ src/Sms.php
@@ -5,20 +5,32 @@ namespace Celysium\Sms;
 use Celysium\Sms\Contracts\SmsInterface;
 use Celysium\Sms\Drivers\Kavenegar;
 use Celysium\Sms\Drivers\Payam;
+use Illuminate\Contracts\Container\Container;
+use Illuminate\Support\Manager;
 
-class Sms
+class Sms extends Manager
 {
+    public function __construct(Container $container)
+    {
+        parent::__construct($container);
+    }
+
     /**
      * @param $driver
      * @return SmsInterface
      */
-    public static function driver($driver = null): SmsInterface
+    public function driver($driver = null): SmsInterface
     {
-        $driver = $driver ?: config('sms.default');
+        $driver = $driver ?: $this->getDefaultDriver();
 
         return match (strtolower($driver)) {
             'kavenegar' => new Kavenegar(),
             'payam' => new Payam()
         };
+    }
+
+    public function getDefaultDriver()
+    {
+        return config('sms.default');
     }
 }
